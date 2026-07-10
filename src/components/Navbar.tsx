4cd11f00531ef79d28eecb38355b296b19a8adc9
usmanmi7 <webworks456@gmail.com>
2026-07-11 02:40:09 +0530

    2.5

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -65,7 +65,7 @@ const Navbar = () => {
       <header className="absolute top-1/2 w-full -translate-y-1/2">
         <nav className="flex size-full items-center justify-between p-4">
           <div className="flex items-center gap-7">
-            <img src="/img/logo.png" alt="logo" className="w-10" />
+            <img src="/img/logo.png" alt="logo" className="w-10 brightness-0 invert" />
             <Button
               id="product-button"
               title="Products"
